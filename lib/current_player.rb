6d19c824273c7bda234cd4e874b77c3d8f51fe27
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-10 21:07:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-=======
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def turn_count(board)
-  counter = 0
-  board.each do |space|
-    if (space == "X" || space == "O")
-      puts "#{counter}"
-      counter += 1
-    end
-  end
-counter
-end
-    else (place != "X" && place != "O")
-      puts "#{counter}"
-    end
-  end
-end
-
-
-
 def current_player(board)
   if turn_count(board).even?
     "X"
