301c275a950367112880c11429ed7407c5625554
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-10 21:02:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,23 +2,20 @@
 =======
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
->>>>>>> 4a6f63be7778af287f184c0eaced6d0c9e9a5cd3
 def turn_count(board)
   counter = 0
   board.each do |space|
     if (space == "X" || space == "O")
       puts "#{counter}"
       counter += 1
-<<<<<<< HEAD
     end
   end
 counter
-=======
+end
     else (place != "X" && place != "O")
       puts "#{counter}"
     end
   end
->>>>>>> 4a6f63be7778af287f184c0eaced6d0c9e9a5cd3
 end
 
 
