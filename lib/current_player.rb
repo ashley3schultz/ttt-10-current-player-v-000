3540bb3af2a580370f5d1ca892aa0b0f56a3af9c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-10 21:05:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -32,3 +32,24 @@ end
     "O"
   end
 end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else turn_count(board).odd?
+    "O"
+  end
+end
+
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if (space == "X" || space == "O")
+      puts "#{counter}"
+      counter += 1
+    end
+  end
+  counter
+end
